3ad5eea495f11031c4571ef3877367b6d6e96e3a
Edgar Herrera <edgarinelfuerte@hotmail.com>
2020-07-16 23:17:26 -0500

    Progress, pending time interval and DBs server register.

--- app.py
+++ app.py
@@ -1,9 +1,34 @@
-from Classes import MongoConnection
-from pymongo import MongoClient
-from pprint import pprint
+import array
+import getpass
+import requests
+import Adafruit_DHT
+import time
 
-_mongoConnection = MongoConnection.MongoConnection.get_instance()
-client: MongoClient = _mongoConnection.client
-db = client.raspberry1
-serverStatusResult = db.command("serverStatus")
-pprint(serverStatusResult)
+DHT_SENSOR = Adafruit_DHT.DHT11
+DHT_PIN = 4
+
+auth = False
+username = input('Ingrese su username: \n')
+password = getpass.getpass('Ingrese su password: \n')
+sensor_id = 0
+
+print(password)
+
+pload = {'username': username, 'password': password}
+rq = requests.post('http://192.168.100.5:3333/users/login', data=pload)
+if rq.status_code == 200:
+    token = rq.json()['token']
+    print('Login exitoso')
+    sensor_id = int(input('Ingrese el sensor_id: \n'))
+    print('\n\n')
+    auth = True
+else:
+    print('no')
+
+if auth:
+    while True:
+        humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
+        if humidity is not None and temperature is not None:
+            print('temperatura: ' + str(temperature))
+            print('humedad: ' + str(humidity))
+            time.sleep(1)
